911c97883db349cbd9091b9cf5f83aa550dcd27b
PHENOMEnON-777 <assolaange.leonard@ictuniversity.edu.cm>
2025-07-07 13:47:56 +0100

    update to use Database url for production

--- core/config.py
+++ core/config.py
@@ -37,12 +37,12 @@ env_path = Path(".") / ".env"
 load_dotenv(dotenv_path=env_path)
 
 class Settings(BaseSettings):
-    DB_USER: str = os.getenv('MYSQL_USER')
-    DB_PASSWORD: str = os.getenv('MYSQL_PASSWORD')  
-    DB_NAME: str = os.getenv('MYSQL_DB')
-    DB_HOST: str = os.getenv('MYSQL_SERVER')
-    DB_PORT: str = os.getenv('MYSQL_PORT')
-    SQLALCHEMY_URL_DATABASE: str = f'mysql+aiomysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}'
+    # DB_USER: str = os.getenv('MYSQL_USER')
+    # DB_PASSWORD: str = os.getenv('MYSQL_PASSWORD')  
+    # DB_NAME: str = os.getenv('MYSQL_DB')
+    # DB_HOST: str = os.getenv('MYSQL_SERVER')
+    # DB_PORT: str = os.getenv('MYSQL_PORT')
+    SQLALCHEMY_URL_DATABASE: str = os.getenv('DATABASE_URL')
     
     SECRET_KEY: str = os.getenv("SECRET_KEY", "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7")
     ALGORITHM: str = os.getenv("ALGORITHM", "HS256")
